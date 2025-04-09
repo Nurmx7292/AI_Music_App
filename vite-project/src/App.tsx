449fd37b7d5f891cd5f87ef6994d983e845b8137
Nurmx7292 <raxmetnur04@gmail.com>
2025-04-09 03:58:28 +0200

    Initial frontend setup

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import { Favorite } from './pages/Favorite'
+import { Trending } from './pages/Trending'
+import { Player } from './pages/Player'
+import { Feed } from './pages/Feed'
+import { Library } from './pages/Library'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Library/>}/>
+        <Route path='/favorite' element={<Favorite/>}/>
+        <Route path='/feed' element={<Feed/>}/>
+        <Route path='/player' element={<Player/>}/>
+        <Route path='/rending' element={<Trending/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
